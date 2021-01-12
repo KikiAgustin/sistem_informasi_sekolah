0072ec8568d92ef9bc864f8989aa5fb56b42d49a
kikiAgustin <kikiagustin62@gmail.com>
2021-01-12 07:05:53 +0600

    menambahkan fitur tambah user

---
+++ application/views/admin/tambah_data_user.php
@@ -0,0 +1,46 @@
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"><?= $judul; ?></h1>
+                    <br>
+
+                    <form role="form" action="" method="POST">
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password-1">Password</label>
+                                <input type="password" name="password-1" class="form-control" id="password-1" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <label for="password-2">Konfirmasi Password</label>
+                                <input type="password" class="form-control" name="password-2" id="password-2" placeholder="Konfirmasi Password">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Tambah Data User</button>
+                        </div>
+
+                </div>
+                <div class="col-sm-6">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+</div>
